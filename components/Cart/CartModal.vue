d40d6e4eb8322de0857823b1bcfdc65b5063cdf9
KeksOvalniy <antonpopov@Mikrocelik.local>
2022-06-19 20:08:12 +0300

    cartmodalstart

---
+++ components/Cart/CartModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex cart-modal">
+    <div class="container w-100">
+      <div class="title m-mb-sm">Ваш заказ</div>
+    </div>
+    <div class="flex align-center justify-between"></div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {},
+}
+</script>
--- components/Header/HeaderBottom.vue
+++ components/Header/HeaderBottom.vue
@@ -1,76 +1,54 @@
 <template>
   <div class="header-bottom">
-    <NuxtLink
-      to="/"
-      class="flex align-center"
-    >
-      <img
-        src="~assets/images/logo.svg"
-        class="logo"
-      >
+    <NuxtLink to="/" class="flex align-center">
+      <img src="~assets/images/logo.svg" class="logo" />
     </NuxtLink>
     <HeaderCategories class="mobile-hide" />
 
     <HelpersSpace></HelpersSpace>
 
-    <Button
-      orange
-      class="mobile-hide"
-      @click="isModalCart = !isModalCart"
-    >
-      <img src="~assets/images/icon/global/cart.svg">
-      {{currPrice}} ₽
+    <Button orange class="mobile-hide" @click="isModalCart = !isModalCart">
+      <img src="~assets/images/icon/global/cart.svg" />
+      {{ currPrice }} ₽
     </Button>
 
-    <Modal
-      v-model="isModalCart"
-      align='right'
-    >
-      <ModalRight />
+    <Modal v-model="isModalCart" align="right">
+      <CartModal />
     </Modal>
 
-    <Button
-      width="32px"
-      class="desktop-hide"
-      @click="toggleMenuMobile"
-    >
-      <div
-        class="header-burger"
-        :class="{'active': menuMobile}"
-      >
+    <Button width="32px" class="desktop-hide" @click="toggleMenuMobile">
+      <div class="header-burger" :class="{ active: menuMobile }">
         <div></div>
         <div></div>
         <div></div>
       </div>
     </Button>
-
   </div>
 </template>
 <script>
 export default {
   computed: {
-    menuMobile () {
+    menuMobile() {
       return this.$store.state.interface.menuMobile
     },
-    currPrice () {
+    currPrice() {
       return this.$store.getters['cart/currPrice']
     },
-
   },
-  data () {
+  data() {
     return {
-      isModalCart: false
+      isModalCart: false,
     }
   },
   methods: {
-    toggleMenuMobile (data) {
+    toggleMenuMobile(data) {
       this.$store.commit('interface/set_menu_mobile', data)
-    }
+    },
   },
   watch: {
-    '$route.name' () {
+    '$route.name'() {
       this.toggleMenuMobile(false)
-    }
-  }
+    },
+  },
 }
 </script>
